bb2975674e63b5c5f881c7d68cd6dc75491f290a
OneDrop1006 <62099536+OneDrop1006@users.noreply.github.com>
2020-03-14 20:03:55 +0900

    add sum function,change func name to tashizanKun (#1)
    * add sum function,change func name to tashizan Kun

--- problem1-twosum/twoSum.py
+++ problem1-twosum/twoSum.py
@@ -18,19 +18,32 @@ class Solution:
     def __init__(self):
         pass
 
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def tashizanKun(self, nums: List[int], target: int) -> List[int]:
         # Write your solution here
-        return [0, 0]
+        n_dict = {}
+        ans_list = []
+        for n in range(len(nums)):
+            n_dict[nums[n]] = n
+        
+        c_dict = n_dict.copy()
+
+        for num,index in n_dict.items():
+            num2 = target - num
+            if num2 in c_dict and num != num2:
+                ans_list.append([index,n_dict[num2]])
+                del c_dict[num]
+                
+        return ans_list
 
 
 def main():
-    print(Solution().twoSum([2, 7, 11, 15], 9))
+    print(Solution().tashizanKun([2, 7, 11, 15], 9))
 
     # more test cases
-    # print(Solution().twoSum([4, 5, 1, 8], 6))  expected answer [1, 2]
-    # print(Solution().twoSum([0, 7, 9, 4, 5, 1, 8], 5))  expected answer [0, 4]
-    # print(Solution().twoSum([3, -4, 8, 11]], 7))  expected answer [1, 3]
-    # print(Solution().twoSum([0, -1, 2, -6, 8, -5, 18]], 12))  expected answer [3, 6]
+    # print(Solution().tashizanKun([4, 5, 1, 8], 6))  expected answer [1, 2]
+    # print(Solution().tashizanKun([0, 7, 9, 4, 5, 1, 8], 5))  expected answer [0, 4]
+    # print(Solution().tashizanKun([3, -4, 8, 11]], 7))  expected answer [1, 3]
+    # print(Solution().tashizanKun([0, -1, 2, -6, 8, -5, 18]], 12))  expected answer [3, 6]
 
 
 if __name__ == '__main__':
